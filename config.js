65f732c9bde541c996640730e87357072e5a4ae9
ryanyustanto748-coder <ryanyustanto748@gmail.com>
2026-06-18 06:28:58 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+let base_api = "https://avdbapi.com/api.php/provide"
+let voe_api_key = "Wr7fjmWTBp6EY0XGYJZwleaMJiJ2cuf21c3UvSpDd7GtPLAVnQTGiY9RNtwCyCbK";
+
+let voe_api = "https://voe.sx/api"
+
+let voe_player = "https://voe.sx/e"
